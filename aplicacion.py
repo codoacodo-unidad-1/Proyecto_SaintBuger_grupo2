9f0077d849df10f2021af8bf2ef26eef84d9ff41
jesypost <101301966+jesypost@users.noreply.github.com>
2023-06-27 19:28:28 -0300

    correccion identacion y agregado de nuevos metodos

--- aplicacion.py
+++ aplicacion.py
@@ -30,11 +30,6 @@ print("\033[H\033[J")
 # -------------------------------------------------------------------
 # Definimos la clase "Inventario"
 # -------------------------------------------------------------------
-class Inventario: 
-    def __init__(self): 
-        self.conexion = get_db_connection() 
-        self.cursor = self.conexion.cursor()     
-        
 class Producto:
     def __init__(self, codigo, descripcion, cantidad, precio):
         self.codigo=codigo
@@ -42,35 +37,54 @@ class Producto:
         self.cantidad=cantidad
         self.precio=precio
 
-def Modificar (self, nueva_descripcion, nueva_cantidad, nuevo_precio):
-    self.descripcion = nueva_descripcion
-    self.cantidad = nueva_cantidad
-    self.precio = nuevo_precio
 
-def agregar_producto(self, codigo, descripcion, cantidad, precio):
-    producto_existente =self.consultar_producto(codigo)
-    if producto_existente:
-        print("Ya existe un producto con este código.")
+class Inventario: 
+    def __init__(self): 
+        self.conexion = get_db_connection() 
+        self.cursor = self.conexion.cursor()  
+        
+    def agregar_producto(self, codigo, descripcion, cantidad, precio):
+        producto_existente =self.consultar_producto(codigo)
+        if producto_existente:
+            print("Ya existe un producto con este código.")
+            return False
+        #nuevo_producto = Producto(codigo, descripcion, cantidad, precio)
+        self.curso.execute("INSERT INTO burguers VALUES (?,?,?,?)" , (codigo,descripcion,cantidad,precio))
+        self.conexion.commit()
+        return True       
+
+
+    def consultar_producto(self,codigo):
+        self.cursor.execute ("SELECT * FROM burguers WHERE codigo= ?" ,  (codigo,))
+        row = self.cursor.fetchone()
+        if row:
+            codigo, descripcion, cantidad, precio = row
+        return Producto(codigo, descripcion,cantidad,precio)
         return False
     
-    nuevo_producto = Producto(codigo, descripcion, cantidad, precio)
-    self.curso.execute("INSERT INTO burguers VALUES (?,?,?,?)" , (codigo,descripcion,cantidad,precio))
-    self.conexion.commit()
-    return True
-
-def consultar_producto(self,codigo):
-    self.cursor.execute ("SELECT * FROM burguers WHERE codigo= ?" ,  (codigo,))
-    row = self.cursor.fetchone()
-    if row:
-        codigo, descripcion, cantidad, precio = row
-        return Producto(codigo, descripcion,cantidad,precio)
-    return False
-    
             
-def modificar_producto(self, codigo,nueva_descripcion,nueva_cantidad,nuevo_precio):
-    producto = self.consultar(codigo)
-    if producto:
-        producto.modificar(nueva_descripcion,nueva_cantidad,nuevo_precio)
-        self.cursor.execute("UPDATE  burguers SET descripcion = ?, cantidad= ?, precio = ? where codigo=?",(nueva_descripcion,nueva_cantidad, nuevo_precio, codigo))
-        self.conexion.commit()
+    def modificar_producto(self, codigo,nueva_descripcion,nueva_cantidad,nuevo_precio):
+        producto = self.consultar(codigo)
+        if producto:
+            producto.modificar(nueva_descripcion,nueva_cantidad,nuevo_precio)
+            self.cursor.execute("UPDATE  burguers SET descripcion = ?, cantidad= ?, precio = ? where codigo=?",(nueva_descripcion,nueva_cantidad, nuevo_precio, codigo))
+            self.conexion.commit()
         
+    def listar_productos(self):
+        print("-"*30)
+        self.cursor.execute("SELECT * FROM burguers")
+        rows= self.cursor.fetchall()
+        for row in rows:
+            codigo,descripcion,cantidad,precio = row
+            print(f"Código:{codigo}")
+            print(f"descripción: {descripcion}")
+            print(f"Cantidad: {precio}")
+            print("-"*30)
+     
+    def eliminar_producto(self,codigo):
+        self.cursor.execute("DELETE FROM burguers WHERE codigo= ?",(codigo,))
+        if self.cursor.rowcount > 0:
+            print("Producto eliminado.")
+            self.conexion.commit()
+        else:
+            print("Producto no encontrado.")
